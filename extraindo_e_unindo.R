d651145680068d315aa08d824705a39db06785cb
prdm0 <pedro.rafael.marinho@gmail.com>
2021-08-27 11:30:07 -0300

    Update

--- extraindo_e_unindo.R
+++ extraindo_e_unindo.R
@@ -1,28 +1,9 @@
-box::use(
-  module/tools_pdf
-)
-
-# Combining slides for the DE
-tools_pdf$pdf_combine(
-  vec_input = 
+staplr::combine_pdf(
+  vec_input =
     c(
-      "/home/prdm0/Dropbox/UFPB/aulas_ufpb/Programação Estatística I/Aula_1.pdf",
-      "/home/prdm0/Dropbox/UFPB/aulas_ufpb/Programação Estatística I/Aula_2.pdf",
-      "/home/prdm0/Dropbox/UFPB/aulas_ufpb/Programação Estatística I/Aula_3.pdf"
+      "/home/prdm0/Downloads/Aula_1.pdf"
     ),
-  output = "./slides_de.pdf",
-  start_pages = c(NA, NA, NA),
-  end_pages = c(NA, NA, 188)  
-)
-
-# Combining slides for the PPGMMC
-tools_pdf$pdf_combine(
-  vec_input = 
-    c(
-      "/home/prdm0/Dropbox/UFPB/aulas_ufpb/Programação Estatística II/Aula_1.pdf",
-      "/home/prdm0/Dropbox/UFPB/aulas_ufpb/Programação Estatística II/Aula_2.pdf"
-    ),
-  output = "./slides_ppgmmc.pdf",
-  start_pages = c(NA, NA),
-  end_pages = c(NA, 120)  
+  output = "/home/prdm0/slides_fracionados/slides_de.pdf",
+  start_pages = c(1L),
+  end_pages = c(140L)
 )
