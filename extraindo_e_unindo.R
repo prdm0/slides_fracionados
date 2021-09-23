755978d2aa800cd05c2a6e7dc4767e818d0a1bd8
prdm0 <pedro.rafael.marinho@gmail.com>
2021-09-23 14:01:49 -0300

    Atualizando slides da aula.

--- extraindo_e_unindo.R
+++ extraindo_e_unindo.R
@@ -1,9 +1,9 @@
-staplr::combine_pdf(
-  vec_input =
+staplr::staple_pages(
+  input_files =
     c(
       "/home/prdm0/Downloads/Aula_1.pdf"
     ),
-  output = "/home/prdm0/slides_fracionados/slides_de.pdf",
+  output_filepath = "/home/prdm0/slides_fracionados/slides_de.pdf",
   start_pages = c(1L),
-  end_pages = c(214L)
+  end_pages = c(255L)
 )
