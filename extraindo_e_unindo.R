e3cab1410345c7e9f12047363d07fb6a1fb61515
prdm0 <pedro.rafael.marinho@gmail.com>
2021-09-30 16:49:50 -0300

    Atualizando aula: Programação em R.

--- extraindo_e_unindo.R
+++ extraindo_e_unindo.R
@@ -4,6 +4,6 @@ staplr::staple_pages(
       "/home/prdm0/Downloads/Aula_1.pdf"
     ),
   output_filepath = "/home/prdm0/slides_fracionados/slides_de.pdf",
-  start_pages = c(1L),
-  end_pages = c(255L)
+  start_pages = c(NA),
+  end_pages = c(NA)
 )
