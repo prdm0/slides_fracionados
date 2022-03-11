fd5c230e56e2b7bc20561cb08ac5b77472965da3
prdm0 <pedro.rafael.marinho@gmail.com>
2022-03-11 07:22:29 -0300

    Atualizado os slides das aulas.

--- extraindo_e_unindo.R
+++ extraindo_e_unindo.R
@@ -1,9 +1,10 @@
 staplr::staple_pages(
   input_files =
     c(
-      "~/Downloads/Aula_1.pdf"
+      "~/Downloads/Aula_1.pdf",
+      "~/Downloads/Aula_2.pdf"
     ),
   output_filepath = "~/github/slides_fracionados/slides_de_c.pdf",
-  start_pages = c(NA),
-  end_pages = c(105)
+  start_pages = c(NA, 1),
+  end_pages = c(NA, 36)
 )
