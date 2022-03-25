ca19deaca3d77dc33bfcb2b5b2c5d4ce0b0b9531
prdm0 <pedro.rafael.marinho@gmail.com>
2022-03-25 13:04:23 -0300

    Atualizando aula de C.

--- extraindo_e_unindo.R
+++ extraindo_e_unindo.R
@@ -1,9 +1,24 @@
+# Programação em C
 staplr::staple_pages(
   input_files =
     c(
-      "~/github/Aula_1.pdf"
+      "~/Dropbox/UFPB/aulas_ufpb/Programação Estatística I/Aula_1.pdf",
+      "~/Dropbox/UFPB/aulas_ufpb/Programação Estatística I/Aula_2.pdf"
     ),
-  output_filepath = "~/github/slides_fracionados/slides_de_r.pdf",
-  start_pages = c(NA),
-  end_pages = c(116)
+  output_filepath = "~/Dropbox/GitHub/slides_fracionados/slides_de_c.pdf",
+  start_pages = c(NA, 1),
+  end_pages = c(NA, 131)
+)
+
+# Programação em R
+
+staplr::staple_pages(
+  input_files =
+    c(
+      "~/Dropbox/UFPB/aulas_ufpb/Programação Estatística II/Aula_1.pdf",
+      "~/Dropbox/UFPB/aulas_ufpb/Programação Estatística II/Aula_2.pdf"
+    ),
+  output_filepath = "~/Dropbox/GitHub/slides_fracionados/slides_de_r.pdf",
+  start_pages = c(NA, 1),
+  end_pages = c(NA, 131)
 )
