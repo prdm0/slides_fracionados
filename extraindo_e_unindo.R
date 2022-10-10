bce92f1cbcb411ffe36a36ae675c9ed2e07ab1f9
prdm0 <pedro.rafael.marinho@gmail.com>
2022-10-10 08:31:29 -0300

    Atualização das aulas de R.

--- extraindo_e_unindo.R
+++ extraindo_e_unindo.R
@@ -21,7 +21,7 @@ staplr::staple_pages(
     ),
   output_filepath = "~/Dropbox/GitHub/slides_fracionados/slides_de_r_de.pdf",
   start_pages = c(NA, NA),
-  end_pages = c(NA, 40)
+  end_pages = c(NA, 66)
 )
 
 # PPGMMC
@@ -34,5 +34,5 @@ staplr::staple_pages(
     ),
   output_filepath = "~/Dropbox/GitHub/slides_fracionados/slides_de_r_ppgmmc.pdf",
   start_pages = c(NA, NA),
-  end_pages = c(NA, 40)
+  end_pages = c(NA, 66)
 )
