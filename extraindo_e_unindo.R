a828836eb0f7c7b10739dc55215bcfdb3a0d922e
prdm0 <pedro.rafael.marinho@gmail.com>
2022-11-11 15:29:20 -0300

    Atualizando slides de R.

--- extraindo_e_unindo.R
+++ extraindo_e_unindo.R
@@ -21,7 +21,7 @@ staplr::staple_pages(
     ),
   output_filepath = "~/Dropbox/GitHub/slides_fracionados/slides_de_r_de.pdf",
   start_pages = c(NA, NA),
-  end_pages = c(NA, 166)
+  end_pages = c(NA, 186)
 )
 
 # PPGMMC
