11793923576d927b7b90c6af0b9836b1b4dc3d1b
prdm0 <pedro.rafael.marinho@gmail.com>
2023-07-28 13:27:37 -0300

    Atualização slides R.

--- extraindo_e_unindo.R
+++ extraindo_e_unindo.R
@@ -1,28 +1,28 @@
 # Programação em C
-staplr::staple_pages(
-  input_files =
-    c(
-      "~/Dropbox/UFPB/aulas_ufpb/Programação Estatística I/Aula_1.pdf",
-      "~/Dropbox/UFPB/aulas_ufpb/Programação Estatística I/Aula_2.pdf",
-      "~/Dropbox/UFPB/aulas_ufpb/Programação Estatística I/Aula_3.pdf"
-    ),
-  output_filepath = "~/Dropbox/GitHub/slides_fracionados/slides_de_c.pdf",
-  start_pages = c(NA, NA, NA),
-  end_pages = c(NA, NA, NA)
-)
-
-# Programação em R
-
 # staplr::staple_pages(
 #   input_files =
 #     c(
-#       "~/Dropbox/UFPB/aulas_ufpb/Programação Estatística II/Aula_1.pdf",
-#       "~/Dropbox/UFPB/aulas_ufpb/Programação Estatística II/Aula_2.pdf"
+#       "~/Dropbox/UFPB/aulas_ufpb/Programação Estatística I/Aula_1.pdf",
+#       "~/Dropbox/UFPB/aulas_ufpb/Programação Estatística I/Aula_2.pdf",
+#       "~/Dropbox/UFPB/aulas_ufpb/Programação Estatística I/Aula_3.pdf"
 #     ),
-#   output_filepath = "~/Dropbox/GitHub/slides_fracionados/slides_de_r_de.pdf",
-#   start_pages = c(NA, NA),
-#   end_pages = c(NA, 186)
+#   output_filepath = "~/Dropbox/GitHub/slides_fracionados/slides_de_c.pdf",
+#   start_pages = c(NA, NA, NA),
+#   end_pages = c(NA, NA, NA)
 # )
+
+# Programação em R
+
+staplr::staple_pages(
+  input_files =
+    c(
+      "~/Dropbox/UFPB/aulas_ufpb/Programação Estatística II/Aula_1.pdf",
+      "~/Dropbox/UFPB/aulas_ufpb/Programação Estatística II/Aula_2.pdf"
+    ),
+  output_filepath = "~/Dropbox/GitHub/slides_fracionados/slides_de_r_de.pdf",
+  start_pages = c(NA, NA),
+  end_pages = c(NA, 200)
+)
 
 # PPGMMC
 
